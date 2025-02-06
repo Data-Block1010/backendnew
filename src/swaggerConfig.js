a522e5366e5b5609e2a525c7b193ad721938920f
Osiyomeoh <samuelaleonomoh5@gmail.com>
2025-02-06 12:44:34 +0300

    chore fix all

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -21,7 +21,7 @@ function setupSwagger(app) {
                     description: 'Development server',
                 },
                 {
-                    url: 'https://backendnew-4hei.onrender.com',
+                    url: 'https://backendnew-wd64.onrender.com',
                     description: 'Production server (Render)',
                 },
             ],
